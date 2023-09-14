2ee9a6bf896aaae5e4c43797f06739e9bd36b84b
chetan <awasthichetan0@gmail.com>
2023-09-14 16:07:56 +0530

    first commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import "./Input.css"
+import { Button } from '@mui/material';
+
+function Input() {
+   const[url,setUrl] = useState('');
+   const[shortenUrl,setShortenUrl] = useState('');
+   const[check,setCheck] = useState(false);
+   const[copy,setCopy] = useState('Copy')
+   const urlChange = (e) =>{
+       setUrl(e.target.value)
+   }
+
+   const shortUrl = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${url}`
+      )
+      const data = await response.json()
+      setShortenUrl(data.result.full_short_link);
+      setCheck(true)
+    } catch (e) {
+      alert(e);
+    }
+};
+
+  const handleClick = () =>{ 
+    navigator.clipboard.writeText(shortenUrl)                  
+    setCopy('Copied')
+  }
+
+  return (
+    <div className='container'>
+        <h1>Paste the Url to be shortened</h1>
+        <div className='wrap'>
+        <input type="text" placeholder='Enter link here' onChange={urlChange}/>
+         <Button className='btn' variant="contained" onClick={shortUrl}type='submit'>submit</Button>
+         <div className='result'>
+         <a target="_blank" href={shortenUrl} rel="noopener noreferrer">{check && shortenUrl}</a>
+     
+        {check && <button className='btn2' onClick={handleClick}>{copy}</button>} 
+         </div>
+         </div>
+    </div>
+  )
+}
+
+export default Input
